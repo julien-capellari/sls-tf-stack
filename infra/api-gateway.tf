fb86061036cdfb0ce5ef3d3fd848bb61a3324cea
Julien Capellari <julien.capellari@neoxia.com>
2022-09-08 12:12:06 +0200

    feat(infra): setup api gateway

---
+++ infra/api-gateway.tf
@@ -0,0 +1,47 @@
+resource "aws_apigatewayv2_api" "todos-api" {
+  name          = "todos-api-tf-stack-${var.stage}"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "todos-lambda-api" {
+  api_id           = aws_apigatewayv2_api.todos-api.id
+  integration_type = "AWS_PROXY"
+
+  connection_type        = "INTERNET"
+  integration_method     = "POST"
+  integration_uri        = aws_lambda_function.lambda-api.invoke_arn
+  passthrough_behavior   = "WHEN_NO_MATCH"
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "todos-lambda-api" {
+  api_id    = aws_apigatewayv2_api.todos-api.id
+  route_key = "ANY /{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.todos-lambda-api.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  name        = "$default"
+  api_id      = aws_apigatewayv2_api.todos-api.id
+  auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.todos-api.arn
+    format          = jsonencode(
+      {
+        httpMethod     = "$context.httpMethod"
+        ip             = "$context.identity.sourceIp"
+        protocol       = "$context.protocol"
+        requestId      = "$context.requestId"
+        requestTime    = "$context.requestTime"
+        responseLength = "$context.responseLength"
+        routeKey       = "$context.routeKey"
+        status         = "$context.status"
+      }
+    )
+  }
+}
+
+output "api_url" {
+  value = aws_apigatewayv2_api.todos-api.api_endpoint
+}
--- infra/lambda.tf
+++ infra/lambda.tf
@@ -2,19 +2,26 @@ resource "aws_lambda_function" "lambda-api" {
   function_name = "todos-api-tf-stack-${var.stage}"
   role          = aws_iam_role.lambda-role.arn
 
-  runtime  = "nodejs16.x"
-  handler  = "lambda.handler"
-  filename = "../backend/dist/lambda.zip"
+  runtime          = "nodejs16.x"
+  handler          = "lambda.handler"
+  filename         = "../backend/dist/lambda.zip"
   source_code_hash = filebase64sha256("../backend/dist/lambda.zip")
 
   environment {
     variables = {
       FRONTEND_URL = "http://localhost:3000"
-      TODO_TABLE = aws_dynamodb_table.todo.name
+      TODO_TABLE   = aws_dynamodb_table.todo.name
     }
   }
 
   tracing_config {
     mode = "Active"
   }
+}
+
+resource "aws_lambda_permission" "lambda-api" {
+  function_name = aws_lambda_function.lambda-api.function_name
+  action        = "lambda:InvokeFunction"
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.todos-api.execution_arn}/*/*/{proxy+}"
 }
